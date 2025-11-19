bfd850959d4778e8eed5e16ca0ba27f6c1363559
Jeremy Fuksa <hello@orangefla.me>
2025-11-19 09:29:44 -0600

    fix: add vite-env.d.ts for image import types

---
+++ src/vite-env.d.ts
@@ -0,0 +1,15 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
